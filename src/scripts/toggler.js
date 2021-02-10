57f2ce18928810e937cd62e5b233bbdbcb818aae
Ann Herbik <45009615+ann-dev@users.noreply.github.com>
2021-02-11 00:52:04 +0100

    No script accordion, animation

--- src/scripts/toggler.js
+++
@@ -1,8 +0,0 @@
-const buttons = document.querySelectorAll('.accordion-button');
-
-function toggleAccordion() {
-    const toggler = this.getAttribute('aria-expanded');
-    toggler == 'false' ? this.setAttribute('aria-expanded', 'true') : this.setAttribute('aria-expanded', 'false')
-}
-
-buttons.forEach((button) => button.addEventListener('click', toggleAccordion, true));
